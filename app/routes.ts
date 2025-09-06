15b07259c8c29206a8cef3bb2621cc6437e6b8e1
Ranjitx000 <133473060+Ranjitx000@users.noreply.github.com>
2025-09-06 21:40:43 +0530

    Authentication

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    {
+        index: true,
+        file: "routes/home.tsx"
+    },
+    {
+        path: "/auth",
+        file: "routes/auth.tsx"
+    }
+] satisfies RouteConfig;
+
+
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,47 @@
+import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "content";
+import ResumeCard from "~/component/ResumeCard";
+import { useEffect } from "react";
+import {  useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ai-resume" },
+    {
+      name: "description",
+      content: "smart feedback from your job application",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } = usePuterStore();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section ">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="  flex flex-wrap gap-4 justify-center ">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
